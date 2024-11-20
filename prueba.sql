f9284704775b858b229320f362fbb7d511e38943
kath0000 <kathiahuamanitavara@gmail.com>
2024-11-20 11:51:11 -0500

    Update prueba.sql

--- prueba.sql
+++ prueba.sql
@@ -1,49 +1,25 @@
-CREATE SCHEMA IF NOT EXISTS PRUEBA;
-USE PRUEBA;
+CREATE SCHEMA IF NOT EXISTS practica_huamani;
+USE practica_huamani;
 
-DROP TABLE IF EXISTS s_cliente;
-CREATE TABLE s_cliente 
- (id                         VARCHAR(3)  NOT NULL,
-  nombre                     VARCHAR(20) NOT NULL,
-  telefono                   VARCHAR(20) NOT NULL,
-  direccion                  VARCHAR(20),
-  ciudad                     VARCHAR(20),
-  provincia                  VARCHAR(15),
-  pais	                      VARCHAR(20),
-  codigo_postal              VARCHAR(15),
-  calificacion_crediticia    VARCHAR(9),
-  representante_ventas_id    VARCHAR(3),
-  region_id                  VARCHAR(3),
-  comentarios                VARCHAR(255),
-  CONSTRAINT s_cliente_id_pk PRIMARY KEY (id),
-  CONSTRAINT s_cliente_calificacion_crediticia_ck
-  CHECK (calificacion_crediticia IN ('EXCELENTE', 'BUENA', 'MALA'))
- );
+DROP TABLE IF EXISTS cliente;
+create table cliente(dni VARCHAR(8) PRIMARY KEY, nombre_cliente VARCHAR(20), apellidos VARCHAR(50), email VARCHAR(50), tipo_cliente VARCHAR(20), localidad VARCHAR(20), direccion_cl VARCHAR(30));
+DROP TABLE IF EXISTS departamento;
+create table departamento (nombre_departamento VARCHAR(20) PRIMARY KEY, email VARCHAR(20));
+DROP TABLE IF EXISTS empleado;
+create table empleado (dni VARCHAR(8) PRIMARY KEY, nombre VARCHAR(20), apellidos VARCHAR(20), email VARCHAR(30), telefono VARCHAR(9), direccion_emp VARCHAR(50), salario DOUBLE, departamento VARCHAR(20), FOREIGN KEY(departamento) REFERENCES departamento(nombre_departamento) ON DELETE CASCADE ON UPDATE CASCADE);
+DROP TABLE IF EXISTS producto;
+create table producto (nombre_producto VARCHAR(20), descripcion VARCHAR(30), tamaño_producto VARCHAR(20), tipo_papel VARCHAR(20), impresion VARCHAR(20), acabado VARCHAR(20), empleado VARCHAR(8), FOREIGN KEY(empleado) REFERENCES empleado(dni) ON DELETE CASCADE ON UPDATE CASCADE);
+DROP TABLE IF EXISTS factura;
+create table factura(n_factura INT PRIMARY KEY, pago VARCHAR(20), importe DOUBLE, envio VARCHAR(20), cliente VARCHAR(8), producto VARCHAR(20), FOREIGN KEY(cliente) REFERENCES cliente(nombre_cliente) ON DELETE CASCADE ON UPDATE CASCADE, FOREIGN KEY(producto) REFERENCES producto(nombre_producto ON DELETE CASCADE ON UPDATE CASCADE);
 
-INSERT INTO s_cliente VALUES ('301', 'Sports,Inc', '540-123-4567','72 High St', 'Harrisonburg', 'VA','US', '22809','EXCELENTE', '12', '1', NULL);
-INSERT INTO s_cliente VALUES ('302', 'Toms Sporting Goods', '540-987-6543','6741 Main St','Harrisonburg', 'VA','US', '22809','MALA', '14', '1', NULL);
-INSERT INTO s_cliente VALUES ('303', 'Athletic Attire', '540-123-6789','54 Market St', 'Harrisonburg', 'VA','US', '22808','BUENA', '14', '1', NULL);
-INSERT INTO s_cliente VALUES ('304', 'Athletics For All', '540-987-1234','286 Main St', 'Harrisonburg', 'VA', 'US', '22808','EXCELENTE', '12', '1', NULL);
-INSERT INTO s_cliente VALUES ('305', 'Shoes for Sports', '540-123-9876','538 High St', 'Harrisonburg', 'VA','US', '22809','EXCELENTE', '14', '1', NULL);
+insert into cliente values('78559966', 'Sofia', 'Diaz Perez', 'sofdp@gmail.com', 'FRECUENTE', 'Santa', 'ca. 23 mz. 2 lt. 3'), ('78229966', 'Carlos', 'Maza Acuña', 'car44@gmail.com', 'FRECUENTE', 'Nvo. Chimbote', 'av. pacifico mz. A2 lt. 23'), ('32449966', 'Ana', 'Alva Bueno', 'anaab@gmail.com', 'POCO FRECUENTE', 'Nv. Chimbote', 'Cipreces mz. T lt. 2');
 
-INSERT INTO s_cliente VALUES ('403', 'Athletics One', '717-234-6786','912 Columbia Rd', 'Lancaster', 'PA','US', '17601','BUENA', '14', '1', NULL);
-INSERT INTO s_cliente VALUES ('404', 'Great Athletes', '717-987-2341','121 Litiz Pike', 'Lancaster', 'PA','US', '17602','EXCELENTE', '12', '1', NULL);
-INSERT INTO s_cliente VALUES ('405', 'Athletics Two', '717-987-9875','435 High Rd', 'Lancaster', 'PA','US', '17602','EXCELENTE', '14', '1', NULL);
-INSERT INTO s_cliente VALUES ('406', 'Athletes Attic', '717-234-9888','101 Greenfield Rd', 'Lancaster', 'PA','US', '17601','MALA', '12', '1', NULL);
+insert into departamento values('INVESTIGACION', 'inve@emp.com'), ('REPORTES', 'inve@emp.com'), ('TAREAS', 'inve@emp.com');
 
-INSERT INTO s_cliente VALUES ('201', 'One Sport', '55-112066222','82 Via Bahia', 'Sao Paolo', NULL, 'Brazil', NULL,'EXCELENTE', '12', '2', NULL);
-INSERT INTO s_cliente VALUES ('202', 'Deportivo Caracas', '58-28066222','31 Sabana Grande', 'Caracas', NULL, 'Venezuela', NULL,'EXCELENTE', '12', '2', NULL);
-INSERT INTO s_cliente VALUES ('203', 'New Delhi Sports', '91-11903338','11368 Chanakya', 'New Delhi', NULL, 'India', NULL,'BUENA', '11', '4', NULL);
-INSERT INTO s_cliente VALUES ('204', 'Ladysport', '1-206-104-0111','281 Queen Street', 'Seattle', 'Washington', 'US', NULL,'EXCELENTE', '11', '1', NULL);
-INSERT INTO s_cliente VALUES ('205', 'Kim''s Sporting Goods', '852-3693888','15 Henessey Road', 'Hong Kong', NULL, NULL, NULL,'EXCELENTE', '11', '4', NULL);
-INSERT INTO s_cliente VALUES ('206', 'Sportique', '33-93425722253','172 Rue de Place', 'Cannes', NULL, 'France', NULL,'EXCELENTE', '13', '5', NULL);
-INSERT INTO s_cliente VALUES ('207', 'Tall Rock Sports', '234-16036222','10 Saint Antoine', 'Lagos', NULL, 'Nigeria', NULL,'BUENA', NULL, '3', NULL);
-INSERT INTO s_cliente VALUES ('208', 'Muench Sports', '49-895274449','435 Gruenestrasse', 'Munich', NULL, 'Germany', NULL,'BUENA', '13', '5', NULL);
+insert into empleado values('78559911', 'Alan', 'Morales Diaz', 'alan@gmail.com', '987654321', 'Santa ca.23 lt. 23',1200, 'INVESTIGACION'), ('78119911', 'Amanda', 'Mora Lia', 'mora@gmail.com', '922654321', 'Av. Pacifico 123',1200, 'TAREAS'), ('22459911', 'Karla', 'Caceres Bueno', 'karc@gmail.com', '987225321', 'Av. Pacifico 553',1100, 'INVESTIGACION');
 
-INSERT INTO s_cliente VALUES ('209', 'Beisbol Si!', '809-352666','415 Playa Del Mar',  'San Pedro de Macoris', NULL, 'Dominican Republic', NULL, 'EXCELENTE', '11', '6', NULL);
-INSERT INTO s_cliente VALUES ('210', 'Futbol Sonora', '52-404555','5 Via Saguaro', 'Nogales', NULL, 'Mexico', NULL,'EXCELENTE', '12', '2', NULL);
-INSERT INTO s_cliente VALUES ('211', 'Helmut''s Sports', '42-2111222','45 Modrany', 'Prague', NULL, 'Czechoslovakia', NULL,'EXCELENTE', '11', '5', NULL);
-INSERT INTO s_cliente VALUES ('212', 'Hamada Sport', '20-31209222','47A Corniche', 'Alexandria', NULL, 'Egypt', NULL,'EXCELENTE', '13', '3', NULL);
-INSERT INTO s_cliente VALUES ('213', 'Sports Emporium', '1-415-555-6281','4783 168th Street', 'San Francisco', 'CA', 'US', NULL,'EXCELENTE', '11', '1', NULL);
-INSERT INTO s_cliente VALUES ('214', 'Sports Retail', '1-716-555-7777','115 Main Street', 'Buffalo', 'NY', 'US', NULL, 'MALA', '11', '1', NULL);
+insert into producto values('PRODUCTO1', 'Tarea2', 'pequeño', 'A4', '2', 'Liso', '78559911'), ('PRODUCTO2', 'Tarea3', 'pequeño', 'A4', '2', 'Mate', '78559911'), ('PRODUCTO3', 'Investigacion3', 'mediano', 'carta', '3', 'Liso', '22459911');
+
+insert into factura values('11', 'EFECTIVO', 50, 'LUNES', '78559966', 'PRODUCTO1'), ('21', 'EFECTIVO', 70, 'LUNES', '78559966', 'PRODUCTO2'), ('22', 'EFECTIVO', 90, 'MARTES', '32449966', 'PRODUCTO3');
+
 COMMIT;
